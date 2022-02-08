50e6dad374ed3c75a5f7882bfa114aa3de3acfaf
DartSams <dartagnansams1@icloud.com>
2022-02-08 11:31:56 -0500

    created login page with css,connected google signin api,added function to recieve frontend data from forms

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 
 //STATIC
 app.use(express.static('public'));
-app.use('/css',express.static(path.join(__dirname,"public/css")));
+app.use('/css',express.static("public/css"));
 app.use('/js',express.static(path.join(__dirname,"public/js")));
 app.use('/img',express.static(path.join(__dirname,"public/img")));
 
@@ -15,15 +15,26 @@ app.use('/img',express.static(path.join(__dirname,"public/img")));
 app.set('views','./views')
 app.set('view engine','ejs')
 
+//middleware allows parsing and recieving frontend data converts to json 
+app.use(express.json());
+app.use(express.urlencoded());
 
 //VIEWS
-app.get('/', (requst,response) => {
+app.get("/", (requst,response) => {
     // response.send("hello world")
     data = {
-        route:"home"
+        route:"login"
     }
-    response.render("test2",data)
+    response.render("login",data)
 });
+
+app.post("/",(req,res) => {
+    console.log(req.body)
+    console.log(req.body)
+    let username = req.body.username
+    let password = req.body.password
+    res.redirect(`/profile/${username}`)
+})
 
 app.get("/profile/:name", (req,res) => {
     data = {
